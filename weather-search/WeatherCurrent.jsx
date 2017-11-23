1e8eb9bc6cd70afcb778bb5243dac17bbb85460e
Angela Li Shuo <als1996@uw.edu>
2017-11-22 16:54:32 -0800

    Update WeatherCurrent.jsx

--- weather-search/WeatherCurrent.jsx
+++ weather-search/WeatherCurrent.jsx
@@ -1,39 +1,21 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-class SavedLocations extends Component {
+class WeatherCurrent extends Component {
     render() {
         return (
-            <div className="panel panel-default">
-                <div className="panel-heading"><h4>Saved Locations</h4></div>
-                <ul className="list-group" id="weather-saved">
-                    {this.props.list.map((item, index) => {
-                        return (
-                            <li className="list-group-item" id="location-saved" key={index}>
-                                <button className="saved-places"
-                                    onClick={e => {
-                                        this.handleWeatherSavedClicked(item);
-                                    }}
-                                >{item}</button>
-                                <button className="remove"
-                                    onClick={e => {
-                                        this.handleRemove(item);
-                                    }}
-                                >Remove</button>
-                            </li>
-                        );
-                    })}
-                </ul>
+
+            <div id="information">
+                <h2>{this.props.location}</h2>
+                <img alt="icon" src={this.props.icon} />
+                <h3>{this.props.temp}°F</h3>
+                <h3>&nbsp;&nbsp;&nbsp;{this.props.shortInfo}&nbsp;</h3>
+                <h3>({this.props.longInfo})</h3>
+
+
             </div>
+
         );
-    }
-
-    handleWeatherSavedClicked(item) {
-        this.props.onWeatherSavedClicked(item);
-    }
-
-    handleRemove(item) {
-        this.props.onRemove(item);
     }
 }
 
-export default SavedLocations;
+export default WeatherCurrent;
