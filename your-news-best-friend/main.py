b7c5159df4cfd97ebd7b0ea3ae2cb3d41b8830e4
angelalishuo <als1996@uw.edu>
2017-12-12 20:06:31 -0800

    added hcde final project code files

--- your-news-best-friend/main.py
+++ your-news-best-friend/main.py
@@ -1,8 +1,6 @@
-#libraries to import
+# libraries to import
 
 import sys, json, webbrowser, jinja2, os, webapp2
-
-
 
 # to handle errors
 
@@ -37,13 +35,13 @@ else:
         return None
 
 
-
 # to prettify json output
 
 def pretty(obj):
     return json.dumps(obj, sort_keys=True, indent=2)
 
-
+api_key = '33d0df534c694b96bd5171ae4b8b6011'
+baseurl = 'https://newsapi.org/v2/top-headlines?'
 
 # news class to store information
 
@@ -55,100 +53,7 @@ class News():
         self.image = newsdict['urlToImage']
 
 
-
-# business news below
-
-api_key = '33d0df534c694b96bd5171ae4b8b6011'
-baseurl = 'https://newsapi.org/v2/top-headlines?'
-def natgeonews(params={}):
-    params['apiKey'] = api_key
-    params['sources'] = 'national-geographic'
-    if format == "json": params["nojsoncallback"]=True
-    url = baseurl + urlencode(params)
-    # print(url)
-    return json.loads(safeGet(url).read())
-
-natgeoarticles = natgeonews()['articles']
-natgeoheadlines = [News(news) for news in natgeoarticles]
-
-
-
-# financial news below
-
-def financialtimes(params={}):
-    params['apiKey'] = api_key
-    params['sources'] = 'financial-times'
-    if format == "json": params["nojsoncallback"]=True
-    url = baseurl + urlencode(params)
-    print(url)
-    return json.loads(safeGet(url).read())
-
-financearticles = financialtimes()['articles']
-financeheadlines = [News(news) for news in financearticles]
-
-
-
-# science news below
-
-def newscientist(params={}):
-    params['apiKey'] = api_key
-    params['sources'] = 'new-scientist'
-    if format == "json": params["nojsoncallback"]=True
-    url = baseurl + urlencode(params)
-    print(url)
-    return json.loads(safeGet(url).read())
-
-sciencearticles = newscientist()['articles']
-scienceheadlines = [News(news) for news in sciencearticles]
-
-
-
-# tech news below
-
-def techcrunch(params={}):
-    params['apiKey'] = api_key
-    params['sources'] = 'techcrunch'
-    if format == "json": params["nojsoncallback"]=True
-    url = baseurl + urlencode(params)
-    print(url)
-    return json.loads(safeGet(url).read())
-
-techarticles = techcrunch()['articles']
-techheadlines = [News(news) for news in techarticles]
-
-
-
-# entertainment news below 
-
-def entertainment(params={}):
-    params['apiKey'] = api_key
-    params['sources'] = 'entertainment-weekly'
-    if format == "json": params["nojsoncallback"]=True
-    url = baseurl + urlencode(params)
-    print(url)
-    return json.loads(safeGet(url).read())
-
-entertainmentarticles = entertainment()['articles']
-entertainmentheadlines = [News(news) for news in entertainmentarticles]
-
-
-
-#sports news below
-
-def sports(params={}):
-    params['apiKey'] = api_key
-    params['sources'] = 'talksport'
-    if format == "json": params["nojsoncallback"]=True
-    url = baseurl + urlencode(params)
-    print(url)
-    return json.loads(safeGet(url).read())
-
-sportsarticles = sports()['articles']
-sportsheadlines = [News(news) for news in sportsarticles]
-
-
-
-# general news below
+# ny times
 
 def general(params={}):
     params['apiKey'] = api_key
@@ -162,6 +67,89 @@ generalarticles = general()['articles']
 generalheadlines = [News(news) for news in generalarticles]
 
 
+# science 
+
+def newscientist(params={}):
+    params['apiKey'] = api_key
+    params['sources'] = 'new-scientist'
+    if format == "json": params["nojsoncallback"]=True
+    url = baseurl + urlencode(params)
+    print(url)
+    return json.loads(safeGet(url).read())
+
+sciencearticles = newscientist()['articles']
+scienceheadlines = [News(news) for news in sciencearticles]
+
+
+# national geographic
+
+def natgeonews(params={}):
+    params['apiKey'] = api_key
+    params['sources'] = 'national-geographic'
+    if format == "json": params["nojsoncallback"]=True
+    url = baseurl + urlencode(params)
+    # print(url)
+    return json.loads(safeGet(url).read())
+
+natgeoarticles = natgeonews()['articles']
+natgeoheadlines = [News(news) for news in natgeoarticles]
+
+
+# business
+
+def financialtimes(params={}):
+    params['apiKey'] = api_key
+    params['sources'] = 'bloomberg'
+    if format == "json": params["nojsoncallback"]=True
+    url = baseurl + urlencode(params)
+    print(url)
+    return json.loads(safeGet(url).read())
+
+financearticles = financialtimes()['articles']
+financeheadlines = [News(news) for news in financearticles]
+
+
+# sports 
+
+def sports(params={}):
+    params['apiKey'] = api_key
+    params['sources'] = 'talksport'
+    if format == "json": params["nojsoncallback"]=True
+    url = baseurl + urlencode(params)
+    print(url)
+    return json.loads(safeGet(url).read())
+
+sportsarticles = sports()['articles']
+sportsheadlines = [News(news) for news in sportsarticles]
+
+
+# entertainment 
+
+def entertainment(params={}):
+    params['apiKey'] = api_key
+    params['sources'] = 'buzzfeed'
+    if format == "json": params["nojsoncallback"]=True
+    url = baseurl + urlencode(params)
+    print(url)
+    return json.loads(safeGet(url).read())
+
+entertainmentarticles = entertainment()['articles']
+entertainmentheadlines = [News(news) for news in entertainmentarticles]
+
+
+# tech 
+
+def techcrunch(params={}):
+    params['apiKey'] = api_key
+    params['sources'] = 'techcrunch'
+    if format == "json": params["nojsoncallback"]=True
+    url = baseurl + urlencode(params)
+    print(url)
+    return json.loads(safeGet(url).read())
+
+techarticles = techcrunch()['articles']
+techheadlines = [News(news) for news in techarticles]
+
 
 # app engine
 
@@ -169,13 +157,14 @@ JINJA_ENVIRONMENT = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.di
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         #gather data into tempatevals dictionary
-        templatevals = {'newscientist': scienceheadlines, 
-                        'natgeonews': natgeoheadlines, 
-                        'financialtimes': financeheadlines,
-                        'sports': sportsheadlines,
-                        'general': generalheadlines,
-                        'entertainment': entertainmentheadlines,
-                        'tech': techheadlines} 
+        results = {'World': generalheadlines,
+                   'Science': scienceheadlines, 
+                   'Geography': natgeoheadlines, 
+                   'Business': financeheadlines,
+                   'Sports': sportsheadlines, 
+                   'Technology': techheadlines,      
+                   'Entertainment': entertainmentheadlines} 
+        templatevals = {"results": results}
         template =  JINJA_ENVIRONMENT.get_template('newstemplate.html')
         self.response.write(template.render(templatevals))
 
