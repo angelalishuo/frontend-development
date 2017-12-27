47ad2db9a3ec1d51cd9a549a5b68f85e87e51b6a
Angela Li Shuo <als1996@uw.edu>
2017-12-26 23:46:38 -0800

    Update search.js

--- trip-tunes/public/js/search.js
+++ trip-tunes/public/js/search.js
@@ -34,45 +34,45 @@ window.addEventListener('load', function (e) {
     	return response.json();
   	}).then(function (userinfo) {	  	
   		user_id = userinfo.id;
-      return fetch('/api/create-playlist?access_token=' + token + "&id=" + user_id + "&name=" + playlistName)
-      .then(function(response) {     		
-      	return response.json();
-      }).then(function(createdplaylist) {
-      	playlist_id = JSON.parse(createdplaylist).id;
-      	return fetch('/api/spotify?q=' + query + '&access_token=' + token)
-      	.then(function(response) {	
-      		return response.json();    
-				}).then(function(search) {
-					var artistID = search.tracks.items[0].artists[0].id;
-					var apiURL;
-					return fetch('/api/recommendations?artistid=' + artistID + '&access_token=' + token)
-					.then(function (response) {
-						return response.json();
-					}).then(function (recs) {
-						var datum = JSON.parse(recs);
-						var dict = datum.tracks;
-						var totaltime = 0;
-						var songs = [];
-						for (var i = 0; i < dict.length; i++) {
-							if (totaltime < duration) {   
-								totaltime += dict[i].duration_ms / 1000
-								songs.push(dict[i].uri);
-							}
+      	return fetch('/api/create-playlist?access_token=' + token + "&id=" + user_id + "&name=" + playlistName)
+      	.then(function(response) {     		
+      		return response.json();
+      	}).then(function(createdplaylist) {
+      		playlist_id = JSON.parse(createdplaylist).id;
+      		return fetch('/api/spotify?q=' + query + '&access_token=' + token)
+      		.then(function(response) {	
+      			return response.json();    
+			}).then(function(search) {
+				var artistID = search.tracks.items[0].artists[0].id;
+				var apiURL;
+				return fetch('/api/recommendations?artistid=' + artistID + '&access_token=' + token)
+				.then(function (response) {
+					return response.json();
+				}).then(function (recs) {
+					var datum = JSON.parse(recs);
+					var dict = datum.tracks;
+					var totaltime = 0;
+					var songs = [];
+					for (var i = 0; i < dict.length; i++) {
+						if (totaltime < duration) {   
+							totaltime += dict[i].duration_ms / 1000
+							songs.push(dict[i].uri);
 						}
-						return fetch('./api/addsongs?uris=' + songs + '&id=' + user_id + "&playlistid=" + playlist_id + '&access_token=' + token)
+					}
+					return fetch('./api/addsongs?uris=' + songs + '&id=' + user_id + "&playlistid=" + playlist_id + '&access_token=' + token)
+					.then(function(response) {
+						return response.json();
+					}).then(function(addedsongs) {
+						return fetch('./api/getplaylist?id=' + user_id + "&playlistid=" + playlist_id + "&access_token=" + token)
 						.then(function(response) {
 							return response.json();
-						}).then(function(addedsongs) {
-							return fetch('./api/getplaylist?id=' + user_id + "&playlistid=" + playlist_id + "&access_token=" + token)
-							.then(function(response) {
-								return response.json();
-							}).then(function(retrievedplaylist) {
-								localStorage.setItem("uritouse", JSON.parse(retrievedplaylist).uri);
-							});
+						}).then(function(retrievedplaylist) {
+							localStorage.setItem("uritouse", JSON.parse(retrievedplaylist).uri);
 						});
 					});
 				});
 			});
-	  });
+		});
+	});
 	}, 3000);
 });
